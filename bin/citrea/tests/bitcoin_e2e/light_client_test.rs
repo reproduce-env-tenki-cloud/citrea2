ac01d3430775d6cf543813f9b57211d62dbdf46e
Erce Can Bektüre <canerce99@hotmail.com>
2024-11-11 16:06:54 +0700

    WIP e2e tests

--- bin/citrea/tests/bitcoin_e2e/light_client_test.rs
+++ bin/citrea/tests/bitcoin_e2e/light_client_test.rs
@@ -115,3 +115,157 @@ async fn test_light_client_proving() -> Result<()> {
         .run()
         .await
 }
+
+struct LightClientProvingMultipleProofsInDaBlock;
+
+impl TestCase for LightClientProvingMultipleProofsInDaBlock {
+    fn test_config() -> TestCaseConfig {
+        TestCaseConfig {
+            with_sequencer: true,
+            with_batch_prover: true,
+            with_light_client_prover: true,
+            ..Default::default()
+        }
+    }
+    fn sequencer_config() -> SequencerConfig {
+        SequencerConfig {
+            min_soft_confirmations_per_commitment: 5,
+            da_update_interval_ms: 500,
+            ..Default::default()
+        }
+    }
+
+    fn batch_prover_config() -> BatchProverConfig {
+        BatchProverConfig {
+            enable_recovery: false,
+            ..Default::default()
+        }
+    }
+
+    fn light_client_prover_config() -> LightClientProverConfig {
+        LightClientProverConfig {
+            enable_recovery: false,
+            ..Default::default()
+        }
+    }
+
+    async fn run_test(&mut self, f: &mut TestFramework) -> Result<()> {
+        let da = f.bitcoin_nodes.get(0).unwrap();
+        let sequencer = f.sequencer.as_ref().unwrap();
+        let batch_prover = f.batch_prover.as_ref().unwrap();
+        // let light_client_prover = f.light_client_prover.as_ref().unwrap();
+
+        let min_soft_confirmations_per_commitment =
+            sequencer.min_soft_confirmations_per_commitment();
+
+        // publish min_soft_confirmations_per_commitment confirmations
+        for _ in 0..min_soft_confirmations_per_commitment {
+            sequencer.client.send_publish_batch_request().await?;
+        }
+        sequencer
+            .wait_for_l2_height(min_soft_confirmations_per_commitment, None)
+            .await?;
+
+        // publish min_soft_confirmations_per_commitment confirmations
+        for _ in min_soft_confirmations_per_commitment..2 * min_soft_confirmations_per_commitment {
+            sequencer.client.send_publish_batch_request().await?;
+        }
+        sequencer
+            .wait_for_l2_height(2 * min_soft_confirmations_per_commitment, None)
+            .await?;
+
+        // Wait for commitment tx to be submitted to DA
+        da.wait_mempool_len(2, Some(TEN_MINS)).await.unwrap();
+
+        // Finalize the DA block which contains the commitment tx
+        da.generate(FINALITY_DEPTH, None).await.unwrap();
+
+        let commitment_l1_height = da.get_finalized_height().await.unwrap();
+
+        // Wait for batch prover to generate proof for commitment
+        batch_prover
+            .wait_for_l1_height(commitment_l1_height, Some(TEN_MINS))
+            .await
+            .unwrap();
+
+        // Assert that commitment is queryable
+        let commitments = batch_prover
+            .client
+            .http_client()
+            .get_sequencer_commitments_on_slot_by_number(commitment_l1_height)
+            .await
+            .unwrap()
+            .unwrap();
+        assert_eq!(commitments.len(), 2);
+
+        println!("\n\n\nHere\n\n\n");
+
+        // Ensure that batch proof is submitted to DA
+        da.wait_mempool_len(2, Some(TEN_MINS)).await.unwrap();
+        Ok(())
+    }
+
+    // async fn run_test(&mut self, f: &mut TestFramework) -> Result<()> {
+    // let da = f.bitcoin_nodes.get(0).unwrap();
+    // let sequencer = f.sequencer.as_ref().unwrap();
+    // let batch_prover = f.batch_prover.as_ref().unwrap();
+    // // let light_client_prover = f.light_client_prover.as_ref().unwrap();
+
+    // let min_soft_confirmations_per_commitment =
+    //     sequencer.min_soft_confirmations_per_commitment();
+
+    // // publish min_soft_confirmations_per_commitment confirmations
+    // for _ in 0..min_soft_confirmations_per_commitment {
+    //     sequencer.client.send_publish_batch_request().await?;
+    // }
+    // sequencer
+    //     .wait_for_l2_height(min_soft_confirmations_per_commitment, None)
+    //     .await?;
+
+    // // publish min_soft_confirmations_per_commitment confirmations
+    // for _ in min_soft_confirmations_per_commitment..2 * min_soft_confirmations_per_commitment {
+    //     sequencer.client.send_publish_batch_request().await?;
+    // }
+    // sequencer
+    //     .wait_for_l2_height(2 * min_soft_confirmations_per_commitment, None)
+    //     .await?;
+
+    // // Wait for commitment tx to be submitted to DA
+    // da.wait_mempool_len(2, Some(TEN_MINS)).await.unwrap();
+
+    // // Finalize the DA block which contains the commitment tx
+    // da.generate(FINALITY_DEPTH, None).await.unwrap();
+
+    // let commitment_l1_height = da.get_finalized_height().await.unwrap();
+
+    // // Wait for batch prover to generate proof for commitment
+    // batch_prover
+    //     .wait_for_l1_height(commitment_l1_height, Some(TEN_MINS))
+    //     .await
+    //     .unwrap();
+
+    // // Assert that commitment is queryable
+    // let commitments = batch_prover
+    //     .client
+    //     .http_client()
+    //     .get_sequencer_commitments_on_slot_by_number(commitment_l1_height)
+    //     .await
+    //     .unwrap()
+    //     .unwrap();
+    // assert_eq!(commitments.len(), 2);
+
+    // println!("\n\n\nHere\n\n\n");
+
+    // // Ensure that batch proof is submitted to DA
+    // da.wait_mempool_len(2, Some(TEN_MINS)).await.unwrap();
+    // Ok(())
+    // }
+}
+
+#[tokio::test]
+async fn test_light_client_proving_with_multiple_proofs_in_da_block() -> Result<()> {
+    TestCaseRunner::new(LightClientProvingMultipleProofsInDaBlock)
+        .set_citrea_path(get_citrea_path())
+        .run()
+        .await
+}
