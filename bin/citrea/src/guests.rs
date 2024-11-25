df44adfa81f3b87ab32cff40063b850c8664fddd
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2024-11-25 14:19:41 +0300

    Use guest macro for DRY

--- bin/citrea/src/guests.rs
+++ bin/citrea/src/guests.rs
@@ -5,69 +5,65 @@ use lazy_static::lazy_static;
 use risc0_binfmt::compute_image_id;
 use sov_rollup_interface::spec::SpecId;
 
+macro_rules! guest {
+    ($a:expr) => {{
+        let code = include_bytes!($a).to_vec();
+        let id = compute_image_id(&code).unwrap();
+
+        (id, code)
+    }};
+}
+
 lazy_static! {
     /// The following 2 are used as latest guest builds for tests that use mock DA.
     pub(crate) static ref BATCH_PROOF_LATEST_MOCK_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
         let mut m = HashMap::new();
-        let code = include_bytes!("../../../target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/batch_proof_mock/batch_proof_mock").to_vec();
-        let id = compute_image_id(&code).unwrap();
 
-        m.insert(SpecId::Genesis, (id, code));
+        m.insert(SpecId::Genesis, guest!("../../../target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/batch_proof_mock/batch_proof_mock"));
         m
     };
     pub(crate) static ref LIGHT_CLIENT_LATEST_MOCK_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
         let mut m = HashMap::new();
-        let code = include_bytes!("../../../target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/light_client_proof_mock/light_client_proof_mock").to_vec();
-        let id = compute_image_id(&code).unwrap();
 
-        m.insert(SpecId::Genesis, (id, code));
+        m.insert(SpecId::Genesis, guest!("../../../target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/light_client_proof_mock/light_client_proof_mock"));
         m
     };
     /// The following 2 are used as latest guest builds for tests that use Bitcoin DA.
     pub(crate) static ref BATCH_PROOF_LATEST_BITCOIN_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
         let mut m = HashMap::new();
-        let code = include_bytes!("../../../target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/batch_proof_bitcoin/batch_proof_bitcoin").to_vec();
-        let id = compute_image_id(&code).unwrap();
 
-        m.insert(SpecId::Genesis, (id, code));
+        m.insert(SpecId::Genesis, guest!("../../../target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/batch_proof_bitcoin/batch_proof_bitcoin"));
         m
     };
     pub(crate) static ref LIGHT_CLIENT_LATEST_BITCOIN_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
         let mut m = HashMap::new();
-        let code = include_bytes!("../../../target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/light_client_proof_bitcoin/light_client_proof_bitcoin").to_vec();
-        let id = compute_image_id(&code).unwrap();
 
-        m.insert(SpecId::Genesis, (id, code));
+        m.insert(SpecId::Genesis, guest!("../../../target/riscv-guest/riscv32im-risc0-zkvm-elf/docker/light_client_proof_bitcoin/light_client_proof_bitcoin"));
         m
     };
     /// Production guests
     pub(crate) static ref BATCH_PROOF_MAINNET_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
         let mut m = HashMap::new();
-        let code = include_bytes!("../../../resources/guests/risc0/mainnet/batch-0.elf").to_vec();
-        let id = compute_image_id(&code).unwrap();
 
-        m.insert(SpecId::Genesis, (id, code));
+        m.insert(SpecId::Genesis, guest!("../../../resources/guests/risc0/mainnet/batch-0.elf"));
         m
     };
     pub(crate) static ref BATCH_PROOF_TESTNET_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
         let mut m = HashMap::new();
-        let code = include_bytes!("../../../resources/guests/risc0/testnet/batch-0.elf").to_vec();
-        let id = compute_image_id(&code).unwrap();
-        m.insert(SpecId::Genesis, (id, code));
+
+        m.insert(SpecId::Genesis, guest!("../../../resources/guests/risc0/testnet/batch-0.elf"));
         m
     };
     pub(crate) static ref LIGHT_CLIENT_MAINNET_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
         let mut m = HashMap::new();
-        let code = include_bytes!("../../../resources/guests/risc0/mainnet/light-0.elf").to_vec();
-        let id = compute_image_id(&code).unwrap();
-        m.insert(SpecId::Genesis, (id, code));
+
+        m.insert(SpecId::Genesis, guest!("../../../resources/guests/risc0/mainnet/light-0.elf"));
         m
     };
     pub(crate) static ref LIGHT_CLIENT_TESTNET_GUESTS: HashMap<SpecId, (Digest, Vec<u8>)> = {
         let mut m = HashMap::new();
-        let code = include_bytes!("../../../resources/guests/risc0/testnet/light-0.elf").to_vec();
-        let id = compute_image_id(&code).unwrap();
-        m.insert(SpecId::Genesis, (id, code));
+
+        m.insert(SpecId::Genesis, guest!("../../../resources/guests/risc0/testnet/light-0.elf"));
         m
     };
 }
