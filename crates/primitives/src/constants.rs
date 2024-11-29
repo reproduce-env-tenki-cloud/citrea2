f36431a70f083737627f0500e6789f0f89c071d6
jfldde <168934971+jfldde@users.noreply.github.com>
2024-11-29 10:23:10 +0000

    Remove prefixes diff

--- crates/primitives/src/constants.rs
+++ crates/primitives/src/constants.rs
@@ -1,24 +1,3 @@
-// Generates a prefix for REVEAL_BATCH_PROOF_PREFIX and REVEAL_LIGHT_CLIENT_PREFIX constants based on compile-time environment.
-// Returns a single-byte prefix [1] if CI_TEST_MODE env var is set, otherwise defaults to [1, 1].
-// This greatly reduces the time required to find a nonce when generating batch proving txs and LightClientTxs
-// We have to make these prefixes constants due to zk proving.
-// But two bytes takes too long to generate nonce, making tests very flaky and slow.
-// So in CI we define an env var CI_TEST_MODE to use less bytes.
-// This doesn't change any method ids, just the prefixes.
-const fn get_reveal_batch_proof_prefix() -> &'static [u8] {
-    match option_env!("CI_TEST_MODE") {
-        Some(v) if matches!(v.as_bytes(), b"1" | b"true") => &[1],
-        _ => &[1, 1],
-    }
-}
-
-const fn get_reveal_light_client_prefix() -> &'static [u8] {
-    match option_env!("CI_TEST_MODE") {
-        Some(v) if matches!(v.as_bytes(), b"1" | b"true") => &[2],
-        _ => &[2, 2],
-    }
-}
-
 const fn get_max_txbody_size() -> usize {
     match option_env!("CI_TEST_MODE") {
         Some(v) if matches!(v.as_bytes(), b"1" | b"true") => 39700,
@@ -27,10 +6,10 @@ const fn get_max_txbody_size() -> usize {
 }
 
 /// Prefix for the reveal transaction ids - batch proof namespace.
-pub const TO_BATCH_PROOF_PREFIX: &[u8] = get_reveal_batch_proof_prefix();
+pub const TO_BATCH_PROOF_PREFIX: &[u8] = &[1, 1];
 
 /// Prefix for the reveal transaction ids - light client namespace.
-pub const TO_LIGHT_CLIENT_PREFIX: &[u8] = get_reveal_light_client_prefix();
+pub const TO_LIGHT_CLIENT_PREFIX: &[u8] = &[2, 2];
 
 pub const TEST_PRIVATE_KEY: &str =
     "1212121212121212121212121212121212121212121212121212121212121212";
