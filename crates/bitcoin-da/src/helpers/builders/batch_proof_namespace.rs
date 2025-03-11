b33aa33eae75b65d3ed9048a886a7ba77947168a
Erce Can Bektüre <canerce99@hotmail.com>
2025-03-11 19:55:11 +0300

    Refactor seq comm nonce finder

--- crates/bitcoin-da/src/helpers/builders/batch_proof_namespace.rs
+++ crates/bitcoin-da/src/helpers/builders/batch_proof_namespace.rs
@@ -225,13 +225,27 @@ pub fn create_batchproof_type_0(
                     },
                 });
             } else {
-                unsigned_commit_tx.output[0]
+                if unsigned_commit_tx.output[0]
                     .value
-                    .checked_sub(Amount::ONE_SAT);
-                unsigned_commit_tx.output[1]
+                    .checked_sub(Amount::ONE_SAT)
+                    .is_none()
+                {
+                    break;
+                }
+                if unsigned_commit_tx.output[1]
                     .value
-                    .checked_add(Amount::ONE_SAT);
-                reveal_tx.output[0].value.checked_sub(Amount::ONE_SAT);
+                    .checked_add(Amount::ONE_SAT)
+                    .is_none()
+                {
+                    break;
+                }
+                if reveal_tx.output[0]
+                    .value
+                    .checked_sub(Amount::ONE_SAT)
+                    .is_none()
+                {
+                    break;
+                }
                 reveal_tx.input[0].previous_output.txid = unsigned_commit_tx.compute_txid();
                 update_witness(
                     &unsigned_commit_tx,
