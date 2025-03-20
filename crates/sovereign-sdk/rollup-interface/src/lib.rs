631b563fcf3c22d9cea41fcd4e002d0f0fbe4985
eyusufatik <esadyusufatik@gmail.com>
2025-03-20 15:58:40 +0300

    update versioning

--- crates/sovereign-sdk/rollup-interface/src/lib.rs
+++ crates/sovereign-sdk/rollup-interface/src/lib.rs
@@ -9,7 +9,7 @@
 ///
 /// Mostly used for web3_clientVersion RPC calls and might be used for other purposes.
 #[cfg(feature = "native")]
-pub const CITREA_VERSION: &str = "v0.6.9";
+pub const CITREA_VERSION: &str = "v0.6.10";
 
 /// Fork module
 pub mod fork;
