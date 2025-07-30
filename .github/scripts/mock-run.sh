eca1ce5e7299d28b6e60828dd6eff499adceb53a
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-07-30 17:20:14 +0300

    edit mockrun

--- .github/scripts/mock-run.sh
+++ .github/scripts/mock-run.sh
@@ -1,10 +1,10 @@
 MAIN_BRANCH=ege/mock-nightly-2
 TEST_DIR=bin/citrea/tests/proving-stats
 
-bash $TEST_DIR/run-test.sh patch-stats.json
+bash ./.github/scripts/run-test.sh patch-stats.json
 git checkout $MAIN_BRANCH
 
 cargo build --features testing
-bash $TEST_DIR/run-test.sh nightly-stats.json
+bash ./.github/scripts/run-test.sh nightly-stats.json
 
 python3 $TEST_DIR/compare-results.py $TEST_DIR/results/patch-stats.json $TEST_DIR/results/nightly-stats.json
