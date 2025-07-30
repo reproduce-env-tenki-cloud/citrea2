1d787a63d5cb597ce8c27039303b1875f1214fce
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-07-30 12:59:34 +0300

    add bash

--- bin/citrea/tests/cycle_counts/mock-run.sh
+++ bin/citrea/tests/cycle_counts/mock-run.sh
@@ -1,10 +1,10 @@
-MAIN_BRANCH=nightly
+MAIN_BRANCH=mock-nightly
 TEST_DIR=bin/citrea/tests/cycle_counts
 
-$TEST_DIR/run-test.sh nightly_stats.json
+bash $TEST_DIR/run-test.sh nightly_stats.json
 git checkout $MAIN_BRANCH
 
 cargo build
-$TEST_DIR/run-test.sh patch_stats.json
+bash $TEST_DIR/run-test.sh patch_stats.json
 
 python3 $TEST_DIR/compare_results.py $TEST_DIR/results/patch_stats.json $TEST_DIR/results/nightly_stats.json
