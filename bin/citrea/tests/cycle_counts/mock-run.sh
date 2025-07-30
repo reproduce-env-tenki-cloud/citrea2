773edaf9bb4b094d422c7e3a1aabe503926024b5
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-07-30 12:58:13 +0300

    add cargo build

--- bin/citrea/tests/cycle_counts/mock-run.sh
+++ bin/citrea/tests/cycle_counts/mock-run.sh
@@ -3,6 +3,8 @@ TEST_DIR=bin/citrea/tests/cycle_counts
 
 $TEST_DIR/run-test.sh nightly_stats.json
 git checkout $MAIN_BRANCH
+
+cargo build
 $TEST_DIR/run-test.sh patch_stats.json
 
 python3 $TEST_DIR/compare_results.py $TEST_DIR/results/patch_stats.json $TEST_DIR/results/nightly_stats.json
