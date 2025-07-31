f0501e7ca351a81b9d764babb5c7984aafe532fc
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-07-31 12:24:53 +0300

    fix container name

--- .github/scripts/run-test.sh
+++ .github/scripts/run-test.sh
@@ -1,11 +1,11 @@
 TEST_DIR=bin/citrea/tests/proving-stats
 OUT_FILE_NAME="$1"
 
-docker compose -f $TEST_DIR/docker-compose.regtest.yml up
+docker compose -f $TEST_DIR/docker-compose.regtest.yml up -d
 sleep 5
 
-docker exec citrea-bitcoin-regtest bitcoin-cli -rpcuser=citrea -rpcpassword=citrea loadwallet sequencer
-docker exec citrea-bitcoin-regtest bitcoin-cli -rpcuser=citrea -rpcpassword=citrea loadwallet batch-prover
+docker exec bitcoin-regtest bitcoin-cli -regtest -rpcuser=citrea -rpcpassword=citrea loadwallet sequencer
+docker exec bitcoin-regtest bitcoin-cli -regtest -rpcuser=citrea -rpcpassword=citrea loadwallet batch-prover
 
 target/debug/citrea --dev --da-layer bitcoin --rollup-config-path $TEST_DIR/configs/sequencer_rollup_config.toml --sequencer $TEST_DIR/configs/sequencer_config.toml --genesis-paths bin/citrea/tests/bitcoin/test-data/gen-proof-input-genesis >> sequencer.log &
 PARALLEL_PROOF_LIMIT=2 target/debug/citrea --dev --da-layer bitcoin --rollup-config-path $TEST_DIR/configs/batch_prover_rollup_config.toml --batch-prover $TEST_DIR/configs/batch_prover_config.toml --genesis-paths bin/citrea/tests/bitcoin/test-data/gen-proof-input-genesis >> batch-prover.log &
