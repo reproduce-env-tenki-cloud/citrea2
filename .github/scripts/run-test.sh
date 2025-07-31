ce89b0120cabeb863310202f24944d0712daa305
Ege Okan Ünaldı <egeokanunaldi@hotmail.com>
2025-07-31 16:21:31 +0300

    make results dir

--- .github/scripts/run-test.sh
+++ .github/scripts/run-test.sh
@@ -10,6 +10,7 @@ docker exec bitcoin-regtest bitcoin-cli -regtest -rpcuser=citrea -rpcpassword=ci
 target/debug/citrea --dev --da-layer bitcoin --rollup-config-path $TEST_DIR/configs/sequencer_rollup_config.toml --sequencer $TEST_DIR/configs/sequencer_config.toml --genesis-paths bin/citrea/tests/bitcoin/test-data/gen-proof-input-genesis >> sequencer.log &
 PARALLEL_PROOF_LIMIT=2 target/debug/citrea --dev --da-layer bitcoin --rollup-config-path $TEST_DIR/configs/batch_prover_rollup_config.toml --batch-prover $TEST_DIR/configs/batch_prover_config.toml --genesis-paths bin/citrea/tests/bitcoin/test-data/gen-proof-input-genesis >> batch-prover.log &
 
+mkdir -p $TEST_DIR/results
 python3 bin/citrea/tests/proving-stats/get-proving-stats.py batch-prover.log $TEST_DIR/results/$OUT_FILE_NAME
 
 pkill citrea
